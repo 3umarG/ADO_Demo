bfb053b351fb27a45873e41e88681f21ad1078d5
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-06-04 04:08:02 +0200

    First Lecture : Connect to Server

--- First/Form1.Designer.cs
+++ First/Form1.Designer.cs
@@ -30,33 +30,57 @@
         {
             btnOpen = new Button();
             btnClose = new Button();
+            northwindDB = new Button();
+            anotherDB = new Button();
             SuspendLayout();
             // 
-            // button1
+            // btnOpen
             // 
             btnOpen.Location = new Point(54, 266);
-            btnOpen.Name = "button1";
+            btnOpen.Name = "btnOpen";
             btnOpen.Size = new Size(144, 55);
             btnOpen.TabIndex = 0;
             btnOpen.Text = "Open Connection";
             btnOpen.UseVisualStyleBackColor = true;
             btnOpen.Click += openConnection;
             // 
-            // button2
+            // btnClose
             // 
             btnClose.Location = new Point(598, 266);
-            btnClose.Name = "button2";
+            btnClose.Name = "btnClose";
             btnClose.Size = new Size(143, 55);
             btnClose.TabIndex = 1;
             btnClose.Text = "Close Connection";
             btnClose.UseVisualStyleBackColor = true;
             btnClose.Click += closeConnection;
             // 
+            // northwindDB
+            // 
+            northwindDB.Location = new Point(345, 182);
+            northwindDB.Name = "northwindDB";
+            northwindDB.Size = new Size(107, 50);
+            northwindDB.TabIndex = 2;
+            northwindDB.Text = "Northwind";
+            northwindDB.UseVisualStyleBackColor = true;
+            northwindDB.Click += button1_Click;
+            // 
+            // anotherDB
+            // 
+            anotherDB.Location = new Point(345, 266);
+            anotherDB.Name = "anotherDB";
+            anotherDB.Size = new Size(107, 35);
+            anotherDB.TabIndex = 3;
+            anotherDB.Text = "Another DB";
+            anotherDB.UseVisualStyleBackColor = true;
+            anotherDB.Click += button1_Click_1;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(anotherDB);
+            Controls.Add(northwindDB);
             Controls.Add(btnClose);
             Controls.Add(btnOpen);
             Name = "Form1";
@@ -68,5 +92,7 @@
 
         private Button btnOpen;
         private Button btnClose;
+        private Button northwindDB;
+        private Button anotherDB;
     }
 }
--- First/Form1.cs
+++ First/Form1.cs
@@ -1,33 +1,69 @@
+using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace First
 {
     public partial class Form1 : Form
     {
+        private SqlConnection sqlConnection;
         public Form1()
         {
             InitializeComponent();
+
+            /// Data Source : the Server you want to connect , . or Server Name that connected to it ==> Local 
+            /// Initial Catalog : Current DB 
+            /// Integrated Securty : true ===> Windows Auth , false ===> SQL Auth     
+            sqlConnection = new SqlConnection(@ConfigurationManager.ConnectionStrings["NorthwindDB"].ConnectionString);
+            
+            
+            // Subscribe for some events : 
+            // 1- when the state of sql connection changed : Closed  ,Opened ...
+            sqlConnection.StateChange += (sender, e) => Text = ($"The SQL connection was : {e.OriginalState} and changed to " +
+                $": {e.CurrentState}");
+
+            // 2- when there is message from Database : like when any SQL command or query failed
+            sqlConnection.InfoMessage += (sender, e) => MessageBox.Show($"Message come from SQL : {e.Message}");
         }
 
-        /// Data Source : the Server you want to connect , . or Server Name that connected to it ==> Local 
-        /// Initial Catalog : Current DB 
-        /// Integrated Securty : true ===> Windows Auth , false ===> SQL Auth
-
-        SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-EF44UM4\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=true");
+      
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+           
         }
 
         private void openConnection(object sender, EventArgs e)
         {
-            sqlConnection.Open();
+            // Check for the connection is closed to prevent the Excpetions
+            if (sqlConnection.State == ConnectionState.Closed)
+                sqlConnection.Open();
+            else this.Text =
+            $"The Connection State is already Opened";
         }
 
         private void closeConnection(object sender, EventArgs e)
         {
-            sqlConnection.Close();
+            if (sqlConnection.State != ConnectionState.Closed)
+                sqlConnection.Close();
+            else this.Text =
+            $"The Connection State is already Closed";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.ChangeDatabase("Northwind");
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.ChangeDatabase("iti");
+            }
         }
     }
 }
